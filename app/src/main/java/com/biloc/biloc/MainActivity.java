e60c64109c2094bcc29513349e817e24477a2bd1
manon.racine1 <manon.racine@he-arc.ch>
2017-10-26 14:47:02 +0200

    - implement fragments - change color styles

--- app/src/main/java/com/biloc/biloc/MainActivity.java
+++ app/src/main/java/com/biloc/biloc/MainActivity.java
@@ -3,6 +3,9 @@ package com.biloc.biloc;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,13 +16,57 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity
+        extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+        MapFragment.OnFragmentInteractionListener,
+        ProfileFragment.OnFragmentInteractionListener,
+        ListFragment.OnFragmentInteractionListener,
+        FavoritesFragment.OnFragmentInteractionListener{
+    String TAG = "testBiloc";
+    ListFragment listFragment;
+    MapFragment mapFragment;
+    ProfileFragment profileFragment;
+    FavoritesFragment favoritesFragment;
+
+    public static final int MAP_DRAWER = 1;
+    public static final int LIST_DRAWER = 2;
+    public static final int PROFILE_DRAWER = 3;
+    public static final int FAVORITES_DRAWER = 4;
+    public static final int MAP_FRAGMENT = 5;
+    public static final int LIST_FRAGMENT = 6;
+    public static final int PROFILE_FRAGMENT = 7;
+    public static final int FAVORITES_FRAGMENT = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        if(findViewById(R.id.fragment_container)!= null){
+            if(savedInstanceState != null){
+                return;
+            }
+            Log.i(TAG, "onCreate: findViewById");
+
+            //listFragment = new ListFragment();
+            mapFragment = new MapFragment();
+            listFragment = new ListFragment();
+            profileFragment = new ProfileFragment();
+            favoritesFragment = new FavoritesFragment();
+
+
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, mapFragment).commit();
+
+        }
+
+
+
+        //-----------------------------------------------------------------------------------
+        // Toolbar / drawer / floating action button
+        //-----------------------------------------------------------------------------------
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -80,22 +127,61 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_map) {
+            onFragmentInteraction(3,MAP_DRAWER);
+        } else if (id == R.id.nav_list) {
+            onFragmentInteraction(3,LIST_DRAWER);
+        } else if (id == R.id.nav_profile) {
+            onFragmentInteraction(3,PROFILE_DRAWER);
+        } else if (id == R.id.nav_favorites) {
+            onFragmentInteraction(3, FAVORITES_DRAWER);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    public void onFragmentInteraction(int position, int fragmentCaller ) {
+
+        Fragment fragmentToCall = null;
+
+        switch (fragmentCaller){
+            case MAP_DRAWER:
+                fragmentToCall = mapFragment;
+                break;
+            case LIST_DRAWER:
+                fragmentToCall = listFragment;
+                break;
+            case PROFILE_DRAWER:
+                fragmentToCall = profileFragment;
+                break;
+            case FAVORITES_DRAWER:
+                fragmentToCall = favoritesFragment;
+                break;
+/*            case MAP_FRAGMENT:
+                fragmentToCall = mapFragment;
+            case LIST_FRAGMENT:
+                fragmentToCall = listFragment;
+            case PROFILE_FRAGMENT:
+                fragmentToCall = mapFragment;
+            case FAVORITES_FRAGMENT:
+                fragmentToCall = mapFragment;*/
+
+        }
+
+        Log.i(TAG, "onCreate: listFragment==null");
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        // Replace whatever is in the fragment_container view with this listFragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, fragmentToCall);
+        transaction.addToBackStack(null);
+        Log.i(TAG, "onCreate: addToBackStack");
+        // Commit the transaction
+        transaction.commit();
+        Log.i(TAG, "onCreate: commit");
+        Log.i(TAG, "onCreate: listFragment!=null");
+    }
+
 }
---
+++ app/src/main/java/com/biloc/biloc/MapFragment.java
@@ -0,0 +1,123 @@
+package com.biloc.biloc;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this listFragment must implement the
+ * {@link MapFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MapFragment#newInstance} factory method to
+ * create an instance of this listFragment.
+ */
+public class MapFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the listFragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private Button mButton;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this listFragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of listFragment MapFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MapFragment newInstance(String param1, String param2) {
+        MapFragment fragment = new MapFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this listFragment
+        View myView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mButton = (Button) myView.findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonPressed(3);
+            }
+        });
+        return myView;
+    }
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(int position) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(position, MainActivity.MAP_FRAGMENT );
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * listFragment to allow an interaction in this listFragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(int position, int fragmentCaller );
+    }
+}
