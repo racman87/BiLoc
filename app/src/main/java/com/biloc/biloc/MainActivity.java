3ae6cbcb61711b049ceb4680efaebf2e4f161521
racman87 <racine.manon@gmail.com>
2017-11-24 21:52:59 +0100

    - Elever la barre du bas et ajouté le bouton pour la position sur la map

--- app/src/main/java/com/biloc/biloc/MainActivity.java
+++ app/src/main/java/com/biloc/biloc/MainActivity.java
@@ -95,7 +95,7 @@ public class MainActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
